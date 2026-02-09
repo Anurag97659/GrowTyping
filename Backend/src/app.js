239eb089b774bb8924237f9bf99bdf5c41e92f79
Anurag Nidhi <anurag.nidhi2005@gmail.com>
2026-02-09 22:01:50 +0530

    cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,12 +7,24 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 
+// app.use(cors({
+//   // origin: process.env.API,
+//   origin:  ['http://localhost:5173', 'https://growtyping.onrender.com'],
+//   credentials: true
+// }));
+// app.options("*", cors());
 app.use(cors({
-  // origin: process.env.API,
-  origin:  ['http://localhost:5173', 'https://growtyping.onrender.com'],
-  credentials: true
+  origin: [
+    'http://localhost:5173',
+    'https://growtyping-1.onrender.com'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.options("*", cors());
+
+
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
